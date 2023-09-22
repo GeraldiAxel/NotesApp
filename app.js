c2e23efb371c08cb9c64be557758f7f55e43a510
GeraldiAxel <geraldi.axel@gmail.com>
2023-09-22 11:30:08 +0700

    setup main files

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 require('dotenv').config();
 
 const express = require('express');
-const expressLayous = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// static files
+app.use(express.static("public"));
+
+// templating engine
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// route
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
