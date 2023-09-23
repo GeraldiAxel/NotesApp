a41e329892c9cc80319641acd73434e1ba0a96f1
GeraldiAxel <geraldi.axel@gmail.com>
2023-09-23 15:29:15 +0700

    add pagination for notes display

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,23 +1,52 @@
+
 const Note = require('../models/Notes');
 const mongoose = require('mongoose');
 
 // get dashboard
-exports.dashboard = async(req, res) => {
-    const locals = {
-        title: "Dashboard",
-        description: "The Best Notes App"
-    }
+exports.dashboard = async (req, res) => {
 
+    let perPage = 3;
+    let page = req.query.page || 1;
+  
+    const locals = {
+      title: "Dashboard",
+      description: "Free NodeJS Notes App.",
+    };
+  
     try {
-        const notes = await Note.find({});
+        const notes = await Note.aggregate([
+            { 
+                $sort: { 
+                    createdAt: -1 
+                } 
+            },
+            { 
+                $match: { 
+                    user: new mongoose.Types.ObjectId(req.user.id) 
+                } 
+            },
+            {
+                $project: {
+                    title: { $substr: ["$title", 0, 30] },
+                    body: { $substr: ["$body", 0, 100] },
+                },
+            },
+        ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+  
+        const count = await Note.count();
+    
         res.render('dashboard/index', {
-            userName: req.user.displayName,
+            userName: req.user.firstName,
             locals,
             notes,
-            layout: '../views/layouts/dashboard'
-        })
-        console.log(notes);
+            layout: "../views/layouts/dashboard",
+            current: page,
+            pages: Math.ceil(count / perPage)
+        });
     } catch (error) {
         console.log(error);
     }
-}
+};
